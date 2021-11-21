18d8e7293ac69e6db9e9e6cf969c3661873f63f4
Calotte <645446746@qq.com>
2021-11-22 01:10:01 +0800

    学习进度：2.3完成

--- app/src/main/java/com/example/myapplication/FirstActivity.java
+++ app/src/main/java/com/example/myapplication/FirstActivity.java
@@ -1,10 +1,13 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,14 +21,44 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
         Button button1 = (Button)findViewById(R.id.button1);
+        // 用例1：显式/隐式Intent
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent("com.example.myapplication.ACTION_START");
+////                Intent intent1 =  new Intent(FirstActivity.this,SecondActivity.class);
+//                intent.addCategory("com.example.myapplication.MY_CATEGORY");
+//                startActivity(intent);
+//            }
+//        });
+//        // 用例2：其他隐式Intent
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_DIAL);//调用系统浏览器
+//                intent.setData(Uri.parse("tel:10086"));
+//                startActivity(intent);
+//            }
+//        });
+        // 用例3、传递数据给下一个活动
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String data = "Hello SecondActivity";
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                intent.putExtra("extra data",data);
+//                startActivity(intent);
+//            }
+//        });
+        // 用例4：返回数据给上一个活动
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("com.example.myapplication.ACTION_START");
-                intent.addCategory("com.example.myapplication.MY_CATEGORY");
-                startActivity(intent);
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);//请求码在onActivityResult中使用
             }
         });
+
     }
 
     @Override
@@ -47,4 +80,22 @@ public class FirstActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returenedData = data.getStringExtra("data return");
+                    Log.d("FirstActivity", returenedData);
+                }
+                break;
+            default:
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+//        Intent intent = getIntent();
+//        String data = intent.getStringExtra("extra data");
+//        Log.d("SecondActivity",data);
+        // 用例4：通过点击按钮返回数据给上一个活动
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data return","hello FirstActivity");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+    // 用例5：通过点击返回键（BACK）返回数据给上一个活动
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        Intent intent = new Intent();
+        intent.putExtra("data return","你好，firstActivity，我从Back键返回");
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
